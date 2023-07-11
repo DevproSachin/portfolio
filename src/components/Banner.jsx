86e6d629aac3fe233ae0391a4a2ad11a231d7c2d
Sachin Sharma <sachusharma233@gmail.com>
2023-07-11 14:22:10 +0530

    changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
   return (
    
     <div className="flex flex-col md:flex-row my-14 justify-between place-items-center  md:px-32 px-5">
-        <div className=" left w-1/2">
+        <div className=" left w-[100%] md:w-1/2">
              <h1 className="text-red-700 text-4xl md:text-8xl  font-extrabold uppercase">{Apptext.hello} {Apptext.name}. </h1>
              <p className="my-5 text-blue-900 text-4xl font-black"><TypeAnimation
   sequence={[
@@ -25,7 +25,7 @@ function Banner() {
              <button className="my-5 bg-red-700  hover:bg-black hover:text-white transition-all rounded-2xl"> Download Resume </button>
            </a>
         </div>
-        <div className="right w-1/2">
+        <div className="right w-[100%] md:w-1/2">
            <img src= {sachin} alt="myimage" className=""/>
         </div>
 
